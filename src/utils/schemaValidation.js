3003d370b874d2281883740232f24ed2fa914f36
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-08-01 13:16:56 +0300

    Fix schemaValidation
    No empty string allowed

--- src/utils/schemaValidation.js
+++ src/utils/schemaValidation.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 
-export const SCHEMA_TASK_BODY = Yup.string()
-  .matches(/^[A-Za-zА-Яа-я0-9^%&,;:'"<>=?$]{1,140}$/,{ excludeEmptyString: true })
-  .required('Add your task');
+export const SCHEMA_TASK_BODY = Yup.object().shape({
+body: Yup.string()
+  .min(1)
+  .max(140)
+  .required()})
