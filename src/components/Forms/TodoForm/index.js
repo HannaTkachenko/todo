15e417c948e7aa6b01f95acfbfc86d12d9c799e3
Hanna Tkachenko <hannatkachenko13@gmail.com>
2022-08-05 23:52:50 +0300

    Fix validation schema
    Delete attempt to make border style for validation

--- src/components/Forms/TodoForm/index.js
+++ src/components/Forms/TodoForm/index.js
@@ -8,12 +8,14 @@ const initialValues = {
   body: "",
   isDone: false,
 };
+
 const TodoForm = (props) => {
   const { addTask } = props;
   const onSubmit = (value, formikBag) => {
     addTask(value);
     formikBag.resetForm();
   };
+
   return (
     <>
       <Formik
@@ -22,21 +24,13 @@ const TodoForm = (props) => {
         validationSchema={SCHEMA_TASK_BODY}
       >
         <Form className={styles.form}>
-          <Field
-            className={styles.inputField}
-            name="body"
-            placeholder="Your task"
-          >
-            {({ field, meta }) => {
-              const inputStyles = cx(styles.input, {
-                [styles.valid]: !meta.error,
-                [styles.invalid]: meta.error,
-              });
-              return <input {...field} className={inputStyles} />;
-            }};
-            <ErrorMessage name={body} component="div" className={styles.error} />
-          </Field>
-          
+          <Field name="body" placeholder="Your task"  />
+
+          <ErrorMessage
+            name={"body"}
+            component="div"
+            className={styles.error}
+          />
           <input className={styles.addBtn} type="submit" value="add" />
         </Form>
       </Formik>
--- src/utils/schemaValidation.js
+++ src/utils/schemaValidation.js
@@ -2,6 +2,5 @@ import * as Yup from "yup";
 
 export const SCHEMA_TASK_BODY = Yup.object().shape({
 body: Yup.string()
-  .min(1,"Enter your task, please" )
-  .max(140)
-  .required()})
+   .matches (/^[^\s][a-zA-Zа-яА-Я0-9\s]{1,140}$/)
+  .required("Enter your task, please")})
